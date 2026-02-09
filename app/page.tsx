3ecf467129e19d1a52f01d0ba05c26fffd54f262
kloupoin3 <kloupoin3@users.noreply.github.com>
2026-02-09 16:48:27 +0900

    fix import exports

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
+import Header from "./components/header";
+import Hero from "./components/hero";
+import VideoSection from "./components/video-section";
+import DailyQuote from "./components/daily-quote";
+import Footer from "./components/footer";
+
 export default function Home() {
-  return <div style={{fontSize: 40}}>HELLO 123</div>;
+  return (
+    <>
+      <Header />
+      <Hero />
+      <VideoSection />
+      <DailyQuote />
+      <Footer />
+    </>
+  );
 }
